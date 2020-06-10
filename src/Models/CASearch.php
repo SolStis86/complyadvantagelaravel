a24c16501024a28cedd3e23b86a1a02d3dee6276
SolStis86 <james@mvdigital.co>
2020-06-10 12:57:15 +0100

    Add data attribute accessor

--- src/Models/CASearch.php
+++ src/Models/CASearch.php
@@ -5,14 +5,26 @@ namespace SolStis86\ComplyAdvantage\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use SolStis86\ComplyAdvantage\ApiClient\ResponseData;
 
 class CASearch extends Model
 {
     protected $casts = [
-        'data' => 'json',
+        'data' => 'array',
     ];
 
     protected $fillable = ['data'];
+
+    public static function createFromApiResponseData(array $response): self
+    {
+        $data = data_get($response, 'content.data', []);
+
+        $search = new static(compact('data'));
+
+        $search->save();
+
+        return $search;
+    }
 
     public function __construct(array $attributes = [])
     {
@@ -26,5 +38,8 @@ class CASearch extends Model
         return $this->morphTo();
     }
 
-
+    public function getDataAttribute($value)
+    {
+        return ResponseData::make(json_decode($value, true));
+    }
 }
