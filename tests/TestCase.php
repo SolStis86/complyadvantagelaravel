9a7712a5c741d362f0490f424d23dcae341bba8f
SolStis86 <james@mvdigital.co>
2020-06-10 13:01:35 +0100

    Database setup for TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,11 +9,34 @@ use SolStis86\ComplyAdvantage\ComplyAdvantageServiceProvider;
 
 abstract class TestCase extends OrchestraTestCase
 {
+    public $runMigrations = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if ($this->runMigrations) {
+            include_once __DIR__.'/../database/migrations/create_ca_searches_table.php.stub';
+
+            (new \CreateCASearchesTables())->up();
+        }
+    }
+
     protected function getEnvironmentSetUp($app)
     {
         parent::getEnvironmentSetUp($app);
 
         $app['config']->set('complyadvantage.api_key', 'DUMMYKEY');
+        $app['config']->set('complyadvantage.database.searches_table_name', 'ca_searches');
+
+        if ($this->runMigrations) {
+            $app['config']->set('database.default', 'testbench');
+            $app['config']->set('database.connections.testbench', [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+            ]);
+        }
     }
 
     protected function getPackageProviders($app)
