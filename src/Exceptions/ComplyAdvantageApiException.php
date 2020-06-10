6ac4c8bc2b56db84cecfc57f71d7fd378fddc0c7
SolStis86 <james@mvdigital.co>
2020-06-10 12:57:47 +0100

    Add response attribute to ComplyAdvantageApiException.php

--- src/Exceptions/ComplyAdvantageApiException.php
+++ src/Exceptions/ComplyAdvantageApiException.php
@@ -5,8 +5,23 @@ namespace SolStis86\ComplyAdvantage\Exceptions;
 
 
 use Exception;
+use Illuminate\Http\Client\Response;
+use Throwable;
 
+/**
+ * Exception ComplyAdvantageApiException
+ */
 class ComplyAdvantageApiException extends Exception
 {
+    /**
+     * @var Response
+     */
+    public $response;
 
+    public function __construct(Response $response, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->response = $response;
+    }
 }
